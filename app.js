e9102b9e972293f525b6351b7c25be817c1b5f5f
Dineth Kariyawasam <dinethkariyawasam@yahoo.com>
2018-05-24 20:04:42 +0530

    index jade file added

--- app.js
+++ app.js
@@ -5,7 +5,12 @@ var app = express();
 var port = process.env.PORT || 5000;
 
 app.use(express.static('public'));
-app.use(express.static('src/views'));
+app.set('views','./src/views');
+app.set('view engine','jade');
+
+app.get('/',function(req,res){
+    res.render('index');
+});
 
 app.get('/category',function(req,res){
     res.send('UCSC_Library Project');
