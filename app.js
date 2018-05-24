dc39db713af1ed3106710078fafb6a7a2267032a
Dineth Kariyawasam <dinethkariyawasam@yahoo.com>
2018-05-24 19:52:11 +0530

    nodemon installed

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ var express = require('express');
 
 var app = express();
 
-var port = 5000;
+var port = process.env.PORT || 5000;
 
 app.use(express.static('public'));
 app.use(express.static('src/views'));
 
-app.get('/',function(req,res){
-    res.send('Hello World');
+app.get('/category',function(req,res){
+    res.send('UCSC_Library Project');
 });
 
 app.listen(port,function(err){
