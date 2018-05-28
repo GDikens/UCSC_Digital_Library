4eb6fba881b04a86a05546d13d1f9b2eef2a3f11
Dineth Kariyawasam <dinethkariyawasam@yahoo.com>
2018-05-28 10:08:43 +0530

    index ejs file added with layouts

--- app.js
+++ app.js
@@ -5,12 +5,18 @@ var app = express();
 var port = process.env.PORT || 5000;
 
 app.use(express.static('public'));
+// app.use(express.static('src/views'));
+
 app.set('views','./src/views');
-app.set('view engine','jade');
+app.set('view engine','ejs');
 
 app.get('/',function(req,res){
-    res.render('index');
+    res.render('index',{title:'Welcome to UCSC Digital Library'});
 });
+
+// app.get('/',function(req,res){
+//     res.send('index');
+// });
 
 app.get('/category',function(req,res){
     res.send('UCSC_Library Project');
