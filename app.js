db2742411055ec8d68390b68ed83c1e30ee7b58d
Dineth Kariyawasam <dinethkariyawasam@yahoo.com>
2018-05-28 17:04:54 +0530

    Gallery route placed

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express();
 
 var port = process.env.PORT || 5000;
+var galleryRouter = express.Router();
 
 app.use(express.static('public'));
 // app.use(express.static('src/views'));
@@ -10,8 +11,24 @@ app.use(express.static('public'));
 app.set('views','./src/views');
 app.set('view engine','ejs');
 
+galleryRouter.route('/')
+    .get(function(req,res){
+        res.send('Hello Gallery');
+    });
+
+app.use('/gallery',galleryRouter);
+
 app.get('/',function(req,res){
-    res.render('index',{title:'Welcome to UCSC Digital Library'});
+    res.render('index',{
+        title:'Welcome to UCSC Digital Library',
+        nav:[{
+            Link:'/gallery',
+            Text: 'Gallery'
+                        },{
+            Link:'/library',
+            Text: 'Library'
+        }]
+    });
 });
 
 // app.get('/',function(req,res){
